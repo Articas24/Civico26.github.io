0c68da1460b5e6b152898d1f53ad37c5ae4a8ab7
Articas24 <stefanoalfonso@hotmail.it>
2026-08-21 22:50:38 +0200

    Refresh finance interaction fix

--- admin-stats-history.js
+++ admin-stats-history.js
@@ -1,5 +1,5 @@
 (()=>{
 'use strict';
 function load(src,onload){const s=document.createElement('script');s.src=src;s.defer=true;if(onload)s.onload=onload;document.head.appendChild(s)}
-load('admin-stats-history-core.js?v=20260821-2235',()=>load('admin-stats-finance.js?v=20260821-2235'));
+load('admin-stats-history-core.js?v=20260821-2248',()=>load('admin-stats-finance.js?v=20260821-2248'));
 })();
