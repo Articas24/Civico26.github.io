fb36b7a531679a4a961b8df6f5564e96265e07f8
Articas24 <stefanoalfonso@hotmail.it>
2026-08-21 20:43:35 +0200

    Refresh admin statistics cache

--- admin-stats-history.js
+++ admin-stats-history.js
@@ -1,5 +1,5 @@
 (()=>{
 'use strict';
 function load(src,onload){const s=document.createElement('script');s.src=src;s.defer=true;if(onload)s.onload=onload;document.head.appendChild(s)}
-load('admin-stats-history-core.js?v=20260821-1905',()=>load('admin-stats-finance.js?v=20260821-1905'));
+load('admin-stats-history-core.js?v=20260821-2034',()=>load('admin-stats-finance.js?v=20260821-2034'));
 })();
